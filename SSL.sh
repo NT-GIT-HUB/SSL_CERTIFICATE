b6e31d09e74b20c497eab347ba9fc3499b9d8c6a
NT-GIT-HUB <nt.injector.com@gmail.com>
2022-05-21 20:24:59 -0300

    up

--- SSL.sh
+++ SSL.sh
@@ -5,11 +5,13 @@ mkdir $HOME/SSL
 cd $HOME/SSL && wget https://raw.githubusercontent.com/NT-GIT-HUB/SSL_CERTIFICATE/main/CONFIG
 read -p "DOMINIO: " -e -i www.google.com dm
 sed 's/www.google.com/'$dm'/g' $HOME/SSL/CONFIG
+sleep 1
 chmod +x $(ls)
 mv $HOME/SSL/CONFIG /etc/nginx/sites-enabled
 rmdir $HOME/SSL
 cd /etc/nginx/
 rm sites-enabled/default
+clear
 certbot --nginx
 crontab -r >/dev/null 2>&1
 (

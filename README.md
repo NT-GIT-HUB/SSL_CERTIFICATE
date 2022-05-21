7aefc21d708ad8284bfcf73a9c35555b471bd283
NT-GIT-HUB <nt.injector.com@gmail.com>
2022-05-21 20:20:29 -0300

    up

---
+++ README.md
@@ -0,0 +1,3 @@
+__Script__
+
+```curl -sL https://raw.githubusercontent.com/NT-GIT-HUB/SSL_CERTIFICATE/main/SSL.sh > SSL.sh; chmod +x SSL.sh; ./SSL.sh```
---
+++ SSL.sh
@@ -0,0 +1,18 @@
+apt-get update -y; apt-get upgrade -y;
+apt-get install nginx certbot python3-certbot-nginx vim ufw -y
+ufw allow https
+mkdir $HOME/SSL
+cd $HOME/SSL && wget https://raw.githubusercontent.com/NT-GIT-HUB/SSL_CERTIFICATE/main/CONFIG
+read -p "DOMINIO: " -e -i www.google.com dm
+sed 's/www.google.com/'$dm'/g' $HOME/SSL/CONFIG
+chmod +x $(ls)
+mv $HOME/SSL/CONFIG /etc/nginx/sites-enabled
+rmdir $HOME/SSL
+cd /etc/nginx/
+rm sites-enabled/default
+certbot --nginx
+crontab -r >/dev/null 2>&1
+(
+	crontab -l 2>/dev/null
+	echo "0 12 * * * /usr/bin/certbot renew --quiet"
+) | crontab -

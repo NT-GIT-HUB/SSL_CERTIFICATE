bef010190ec09c06c8bf5cdfafe7f83b098b82bb
NT-GIT-HUB <nt.injector.com@gmail.com>
2022-05-21 21:38:27 -0300

    up

--- SSL.sh
+++ SSL.sh
@@ -1,5 +1,39 @@
-apt-get update -y; apt-get upgrade -y;
-apt-get install nginx certbot python3-certbot-nginx vim ufw -y
+#!/bin/bash
+clear
+fun_bar () {
+comando[0]="$1"
+comando[1]="$2"
+(
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+) > /dev/null 2>&1 &
+tput civis
+echo -ne "  \033[1;33mAGUARDE \033[1;37m- \033[1;33m["
+while true; do
+for((i=0; i<18; i++)); do
+echo -ne "\033[1;31m#"
+sleep 0.1s
+done
+[[ -e $HOME/fim ]] && rm $HOME/fim && break
+echo -e "\033[1;33m]"
+sleep 1s
+tput cuu1
+tput dl1
+echo -ne "  \033[1;33mAGUARDE \033[1;37m- \033[1;33m["
+done
+echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+tput cnorm
+}
+
+inst_pct () {
+_pacotes=("nginx" "certbot" "python3-certbot-nginx" "vim" "ufw")
+for _prog in ${_pacotes[@]}; do
+apt install $_prog -y
+done
+}
+fun_bar 'inst_pct'
 ufw allow https
 cd /etc/nginx/sites-enabled && wget https://raw.githubusercontent.com/NT-GIT-HUB/SSL_CERTIFICATE/main/CONFIG
 read -p "DOMINIO: " -e -i www.google.com dm

5985c81be0a27c59990c752130108e6e2fafbcf6
NT-GIT-HUB <nt.injector.com@gmail.com>
2022-05-21 22:18:20 -0300

    up

--- SSL.sh
+++ SSL.sh
@@ -3,8 +3,9 @@ clear
 echo -e ""
 echo -e "  \033[1;36mCERTIFICADO SSL PARA SEU DOMINIO\033[1;37m"
 echo -e ""
-echo -e "  \033[1;31m• \033[1;33mCRIAR CERTIFICADO SSL[N/S]\033[0m: "; read ok
+echo -e "  \033[1;31m• \033[1;33mCRIAR CERTIFICADO SSL [N/S]\033[0m: "; read ok
 [[ $ok = @(n|N) ]] && exit
+clear
 echo -e ""
 fun_bar () {
 comando[0]="$1"
